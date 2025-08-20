0a86d914e81155079c2c8b705016b24af271aa55
Datta-Khade <datta.khade@safe-lanes.com>
2025-08-20 17:52:22 +0530

    lk

--- src/pages/Table/AggridTable.tsx
+++ src/pages/Table/AggridTable.tsx
@@ -120,7 +120,9 @@ const AgGridPlayground: React.FC = () => {
       </div>
 
       {/* Grid */}
-      <AgGridTable rowData={rowData} columnDefs={columnDefs} licenseKey="" {...options} />
+      <AgGridTable rowData={rowData} columnDefs={columnDefs} 
+       licenseKey="Using_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-090368}_in_excess_of_the_licence_granted_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_changing_this_key_please_contact_info@ag-grid.com___{Safe_Lanes_Consultants_Pte_Ltd}_is_granted_a_{Single_Application}_Developer_License_for_the_application_{SAIL}_only_for_{1}_Front-End_JavaScript_developer___All_Front-End_JavaScript_developers_working_on_{SAIL}_need_to_be_licensed___{SAIL}_has_been_granted_a_Deployment_License_Add-on_for_{1}_Production_Environment___This_key_works_with_{AG_Charts_and_AG_Grid}_Enterprise_versions_released_before_{23_September_2025}____[v3]_[0102]_MTc1ODU4MjAwMDAwMA==461929539170ec991c51dbb3cb8da123"
+      {...options} />
     </div>
   );
 };
